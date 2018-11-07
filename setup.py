2593a32d8193b8e8b3360ca9c52190718f364a5c
Mark S. Bentley <mark@lunartech.org>
2018-11-07 11:05:34 +0100

    Updating package structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='liggghts-utils',
+      version='0.1',
+      description='LIGGGHTS utilities',
+      author='Mark S. Bentley',
+      author_email='mark@lunartech.org',
+      packages=['liggghts-utils'],
+      zip_safe=False)
+
