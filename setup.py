a110320097657ae3c39737553744adc411cde67c
Mark S. Bentley <mark@lunartech.org>
2019-09-13 16:21:20 +0200

    Complete package renaming

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='liggghts-utils',
+setup(name='liggghts_utils',
       version='0.1',
       description='LIGGGHTS utilities',
       author='Mark S. Bentley',
       author_email='mark@lunartech.org',
-      packages=['liggghts-utils'],
+      packages=['liggghts_utils'],
       zip_safe=False)
 
